57ce0fee2426f98c4317b64945ece275ad23c917
Fressia <ydg7736@naver.com>
2020-12-13 19:49:36 +0900

    commit

--- instance.tf
+++ instance.tf
@@ -1,20 +1,19 @@
 // Configure the Google Cloud provider
 provider "google" {
   credentials = file("credentials.json")
-
   project     = "strong-phalanx-264015"
   region      = "us-central1"
 }
 
 // Create a GCE instance
 resource "google_compute_instance" "default" {
-  name         = "terraform"
-  machine_type = "f1-micro"
+  name         = "terraform-test"
+  machine_type = "e2-micro"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-cloud/centos-7"
     }
   }
 
